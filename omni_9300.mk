f0b968f7f6e68aec9b81103a247e5f09dc437e1d
wyuweng <yuweng@hotmail.com>
2016-04-05 15:13:10 +0800

    first commit

---
+++ omni_9300.mk
@@ -0,0 +1,12 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Time Zone data Credits to desalesouche => http://forum.xda-developers.com/showpost.php?p=64850444&postcount=1204
+#PRODUCT_COPY_FILES += \
+#bionic/libc/zoneinfo/tzdata:recovery/root/system/usr/share/zoneinfo/tzdata
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_MODEL="i9300" \
+    TARGET_DEVICE="bird77_a_cu_ics2"
+
+PRODUCT_NAME := omni_9300
+PRODUCT_DEVICE := 9300
